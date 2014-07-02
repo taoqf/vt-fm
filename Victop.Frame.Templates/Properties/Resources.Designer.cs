45fc1cb1b664b2f938c85282ebe34952600bc9e4
wangzhaoguang <wangzhaoguang@victop.com>
2014-07-02 09:17:47 +0800

    数据引用项目修改为框架模版

--- Victop.Frame.Templates/Properties/Resources.Designer.cs
+++ Victop.Frame.Templates/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Victop.Frame.DataReference.Properties {
+namespace Victop.Frame.Templates.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Victop.Frame.DataReference.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Victop.Frame.DataReference.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Victop.Frame.Templates.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- Victop.Frame.Templates/Properties/Settings.Designer.cs
+++ Victop.Frame.Templates/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Victop.Frame.DataReference.Properties {
+namespace Victop.Frame.Templates.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
