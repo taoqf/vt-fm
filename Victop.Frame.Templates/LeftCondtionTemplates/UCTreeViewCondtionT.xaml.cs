96e0fc1ce2b37db5657ddaf74f8dc4f8da9a13fb
zhaoxiaohuan <zhaoxiaohuan@victop.com>
2014-07-04 11:26:03 +0800

    工具栏按钮
    TreeView

--- Victop.Frame.Templates/LeftCondtionTemplates/UCTreeViewCondtionT.xaml.cs
+++ Victop.Frame.Templates/LeftCondtionTemplates/UCTreeViewCondtionT.xaml.cs
@@ -19,9 +19,58 @@ namespace Victop.Frame.Templates.LeftCondtionTemplates
     /// </summary>
     public partial class UCTreeViewCondtionT : UserControl
     {
+        #region TreeView委托及事件
+        // <summary>
+        /// TreeView委托
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private delegate void TreeViewDelegate(object sender, EventArgs e);
+        /// <summary>
+        /// TreeView加载事件。
+        /// </summary>
+        public event TreeViewDelegate TreeViewLoad;
+        public event TreeViewDelegate TreeViewSelectedItemChanged;
+        #endregion
+
+        #region 无参构造函数
+        /// <summary>
+        /// 构造函数
+        /// </summary>
         public UCTreeViewCondtionT()
         {
             InitializeComponent();
-        }
+        } 
+        #endregion
+
+        #region TreeView加载事件
+        /// <summary>
+        /// TreeView加载事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void tviewEmployee_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (TreeViewLoad != null)
+            {
+                TreeViewLoad(sender, e);
+            }
+        } 
+        #endregion
+
+        #region TreeView选择项改变事件
+        /// <summary>
+        /// TreeView选择项改变事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void tviewEmployee_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            if (TreeViewSelectedItemChanged != null)
+            {
+                TreeViewSelectedItemChanged(sender, e);
+            }
+        } 
+        #endregion
     }
 }
--- Victop.Frame.Templates/ToolBarTemplates/UCSearchToolBarT.xaml.cs
+++ Victop.Frame.Templates/ToolBarTemplates/UCSearchToolBarT.xaml.cs
@@ -19,9 +19,119 @@ namespace Victop.Frame.Templates.ToolBarTemplates
     /// </summary>
     public partial class UCSearchToolBarT : UserControl
     {
+        #region 委托及事件
+        /// <summary>
+        /// 按钮委托
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private delegate void ButtonDelegate(object sender, EventArgs e);
+        /// <summary>
+        /// 新增按钮点击事件
+        /// </summary>
+        public event ButtonDelegate btnAddClick;
+        /// <summary>
+        /// 删除按钮点击事件
+        /// </summary>
+        public event ButtonDelegate btnDeleteClick;
+        /// <summary>
+        /// 取消按钮点击事件
+        /// </summary>
+        public event ButtonDelegate btnCancelClick;
+        /// <summary>
+        /// 全消按钮点击事件
+        /// </summary>
+        public event ButtonDelegate btnCancelAllClick;
+        /// <summary>
+        /// 保存按钮点击事件
+        /// </summary>
+        public event ButtonDelegate btnSaveClick; 
+        #endregion
+
+        #region 无参构造函数
+        /// <summary>
+        /// 构造函数
+        /// </summary>
         public UCSearchToolBarT()
         {
             InitializeComponent();
-        }
+        } 
+        #endregion
+
+        #region 新增按钮单击
+        /// <summary>
+        /// 新增按钮单击
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            if (btnAddClick!=null)
+            {
+                btnAddClick(sender,e);
+            }
+        } 
+        #endregion
+
+        #region 删除按钮点击
+        /// <summary>
+        /// 删除按钮点击
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            if (btnDeleteClick!=null)
+            {
+                btnDeleteClick(sender, e);
+            }
+        } 
+        #endregion
+
+        #region 取消按钮单击
+        /// <summary>
+        /// 取消按钮单击
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            if (btnCancelClick!=null)
+            {
+                btnCancelClick(sender, e);
+            }
+        } 
+        #endregion
+
+        #region 全消按钮单击
+        /// <summary>
+        /// 全消按钮单击
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnCancelAll_Click(object sender, RoutedEventArgs e)
+        {
+            if (btnCancelAllClick!=null)
+            {
+                btnCancelAllClick(sender, e);
+            }
+        } 
+        #endregion
+
+        #region 保存按钮单击
+        /// <summary>
+        /// 保存按钮单击
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            if (btnSaveClick!=null)
+            {
+                btnSaveClick(sender, e);
+            }
+        } 
+        #endregion
+       
     }
 }
