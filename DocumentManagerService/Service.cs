1342077e87b69d55f3a97346d057983bb9832592
wangzhaoguang <wangzhaoguang@victop.com>
2014-07-14 08:34:42 +0800

    文件管理服务项目创建签入

---
+++ DocumentManagerService/Service.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using Victop.Server.Controls;
+
+namespace DocumentManagerService
+{
+    public class Service : IService
+    {
+        /// <summary>
+        /// 自动加载
+        /// </summary>
+        public int AutoInit
+        {
+            get { return 0; }
+        }
+
+        public string ServiceName
+        {
+            get { return "DocumentManagerService"; }
+        }
+
+        private List<string> serviceReceiptMessageType;
+
+        public List<string> ServiceReceiptMessageType
+        {
+            get
+            {
+                if (serviceReceiptMessageType == null)
+                {
+                    serviceReceiptMessageType = new List<string>();
+                    //TODO:添加消息类型
+                }
+                return serviceReceiptMessageType;
+            }
+        }
+
+        public string CurrentMessageType
+        {
+            get;
+            set;
+        }
+
+        public string ServiceParams
+        {
+            get;
+            set;
+        }
+
+        public string ServiceDescription
+        {
+            get { return "文件管理服务"; }
+        }
+
+        public bool ServiceRun()
+        {
+            bool result = false;
+            try
+            {
+                WebClient webClient = new WebClient();
+                
+            }
+            catch (Exception ex)
+            {
+               
+            }
+            return result;
+        }
+    }
+}
