c2b26109b049b0efdba2199a7a425351fd38d903
wangzhaoguang <wangzhaoguang@victop.com>
2014-07-29 11:22:13 +0800

    增加JSON格式与DataTable转换的方法

--- Victop.Frame.DataChannel/DataTool.cs
+++ Victop.Frame.DataChannel/DataTool.cs
@@ -6,12 +6,14 @@ using System.Xml;
 using System.IO;
 using System.Data;
 using System.Text.RegularExpressions;
+using System.Collections;
+using Victop.Frame.PublicLib.Helpers;
 
 namespace Victop.Frame.DataChannel
 {
-   internal class DataTool
+    internal class DataTool
     {
-       
+
         /// <summary>
         /// 将xml对象内容字符串转换为DataSet
         /// </summary>
@@ -28,7 +30,7 @@ namespace Victop.Frame.DataChannel
                 //从stream装载到XmlTextReader
                 reader = new XmlTextReader(stream);
                 xmlDS.ReadXml(reader);
-                
+
                 return xmlDS;
             }
             catch (System.Exception ex)
@@ -41,32 +43,32 @@ namespace Victop.Frame.DataChannel
             }
         }
 
-      /// <summary>
+        /// <summary>
         /// 正规表达式,匹配并转换带T的时间格式
-      /// </summary>
-      /// <param name="source"></param>
-      /// <returns></returns>
-       private static string ChangeDateTimeFormate(string source)
-       {
-           string rule = "\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{5}";
-           if (Regex.IsMatch(source, rule))
-           {
-               StringBuilder stringBuilder = new StringBuilder();
-               string[] str = Regex.Split(source, rule);
-               stringBuilder.Append(str[0]);
-               MatchCollection results = Regex.Matches(source, rule);
-               for (int i = 0; i < results.Count; i++)
-               {
-                   stringBuilder.Append(results[i].Value.Replace("T", " ").Substring(0, 19));
-                   stringBuilder.Append(str[i + 1]);
-               }
-              return stringBuilder.ToString();
-           }
-           else
-           {
-               return source;
-           }
-       }
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        private static string ChangeDateTimeFormate(string source)
+        {
+            string rule = "\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{5}";
+            if (Regex.IsMatch(source, rule))
+            {
+                StringBuilder stringBuilder = new StringBuilder();
+                string[] str = Regex.Split(source, rule);
+                stringBuilder.Append(str[0]);
+                MatchCollection results = Regex.Matches(source, rule);
+                for (int i = 0; i < results.Count; i++)
+                {
+                    stringBuilder.Append(results[i].Value.Replace("T", " ").Substring(0, 19));
+                    stringBuilder.Append(str[i + 1]);
+                }
+                return stringBuilder.ToString();
+            }
+            else
+            {
+                return source;
+            }
+        }
 
         /// <summary>
         ///  将DataSet转换为xml
@@ -174,7 +176,7 @@ namespace Victop.Frame.DataChannel
                 if (!tmpList.Contains(((XmlElement)node).GetAttribute("attrname")))
                 {
                     //stringBuilder.Append(" <xs:attribute name=\"").Append(((XmlElement)node).GetAttribute("attrname")).Append( "msdata:Caption=测试").Append("\" type=\"xs:string\"/>");
-                    stringBuilder.AppendFormat("<xs:attribute name=\"{0}\" msdata:Caption=\"{1}\" type=\"xs:string\"/>", ((XmlElement)node).GetAttribute("attrname"),"测试");
+                    stringBuilder.AppendFormat("<xs:attribute name=\"{0}\" msdata:Caption=\"{1}\" type=\"xs:string\"/>", ((XmlElement)node).GetAttribute("attrname"), "测试");
                     tmpList.Add(((XmlElement)node).GetAttribute("attrname"));
                 }
             }
@@ -206,6 +208,73 @@ namespace Victop.Frame.DataChannel
 
             return xmlString;
         }
+
+
+        #region JSON与DataTable的转换
+        /// <summary>
+        /// JSON Array转换成DataTable
+        /// </summary>
+        /// <param name="jsonStr"></param>
+        /// <returns></returns>
+        public static DataTable JsonToDataTable(string jsonStr)
+        {
+            DataTable dataTable = new DataTable();  //实例化
+            DataTable result;
+            try
+            {
+                ArrayList arrayList = JsonHelper.ToObject<ArrayList>(jsonStr);
+                if (arrayList.Count > 0)
+                {
+                    foreach (Dictionary<string, object> dictionary in arrayList)
+                    {
+                        if (dictionary.Keys.Count<string>() == 0)
+                        {
+                            result = dataTable;
+                            return result;
+                        }
+                        if (dataTable.Columns.Count == 0)
+                        {
+                            foreach (string current in dictionary.Keys)
+                            {
+                                dataTable.Columns.Add(current, dictionary[current].GetType());
+                            }
+                        }
+                        DataRow dataRow = dataTable.NewRow();
+                        foreach (string current in dictionary.Keys)
+                        {
+                            dataRow[current] = dictionary[current];
+                        }
+                        dataTable.Rows.Add(dataRow); //循环添加行到DataTable中
+                    }
+                }
+            }
+            catch
+            {
+            }
+            result = dataTable;
+            return result;
+        }
+        /// <summary>
+        /// DataTable转换为Json Array
+        /// </summary>
+        /// <param name="jsonDt"></param>
+        /// <returns></returns>
+        public static string DataTableToJson(DataTable jsonDt)
+        {
+            ArrayList arrayList = new ArrayList();
+            foreach (DataRow dataRow in jsonDt.Rows)
+            {
+                Dictionary<string, object> dictionary = new Dictionary<string, object>();  //实例化一个参数集合
+                foreach (DataColumn dataColumn in jsonDt.Columns)
+                {
+                    dictionary.Add(dataColumn.ColumnName, dataRow[dataColumn.ColumnName].ToString());
+                }
+                arrayList.Add(dictionary); //ArrayList集合中添加键值
+            }
+            return JsonHelper.ToJson(arrayList);
+        }
+        #endregion
+
 
     }
 }
