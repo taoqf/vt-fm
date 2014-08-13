b8029f5e66fbd3c333206929980d4e067b61a331
wangzhaoguang <wangzhaoguang@victop.com>
2014-08-13 12:11:50 +0800

    截获数据处理的异常，并附加到应答消息中

--- Victop.Frame.Connection/ReplyMessageResolver.cs
+++ Victop.Frame.Connection/ReplyMessageResolver.cs
@@ -15,66 +15,78 @@ namespace Victop.Frame.Connection
     using Victop.Frame.CoreLibrary.Models;
     using Victop.Frame.DataChannel;
     using Victop.Frame.PublicLib.Helpers;
-	/// <summary>
-	/// 应答消息解析器
-	/// </summary>
-	/// <remarks>应答消息解析器</remarks>
-	public class ReplyMessageResolver
-	{
-		/// <summary>
-		/// 应答消息数据部分
-		/// </summary>
-		private ReplyMessage DataReplyMessage
-		{
-			get;
-			set;
-		}
+    /// <summary>
+    /// 应答消息解析器
+    /// </summary>
+    /// <remarks>应答消息解析器</remarks>
+    public class ReplyMessageResolver
+    {
+        /// <summary>
+        /// 应答消息数据部分
+        /// </summary>
+        private ReplyMessage DataReplyMessage
+        {
+            get;
+            set;
+        }
 
-		/// <summary>
-		/// 应答消息状态部分
-		/// </summary>
+        /// <summary>
+        /// 应答消息状态部分
+        /// </summary>
         private ReplyMessage MessageStatusReplyMessage
-		{
-			get;
-			set;
-		}
+        {
+            get;
+            set;
+        }
 
-		/// <summary>
-		/// 分解应答消息体的数据部分和状态部分
-		/// </summary>
+        /// <summary>
+        /// 分解应答消息体的数据部分和状态部分
+        /// </summary>
         public virtual ReplyMessage ResolveReplyMessage(ReplyMessage replyMessageInfo, RequestMessage messageInfo)
-		{
+        {
             DataReplyMessage = new ReplyMessage()
             {
                 MessageId = messageInfo.MessageId,
                 ReplyContent = replyMessageInfo.ReplyContent
             };
             string channelId = string.Empty;
-            if (ReplyToDataChannel(DataReplyMessage,messageInfo,out channelId))
+            try
             {
-                MessageStatusReplyMessage = new ReplyMessage()
+                if (ReplyToDataChannel(DataReplyMessage, messageInfo, out channelId))
                 {
-                    MessageId = messageInfo.MessageId,
-                    DataChannelId = channelId,
-                    ReplyMode = ReplyModeEnum.SYNCH
-                };
-                return MessageStatusReplyMessage;
+                    MessageStatusReplyMessage = new ReplyMessage()
+                    {
+                        MessageId = messageInfo.MessageId,
+                        DataChannelId = channelId,
+                        ReplyMode = ReplyModeEnum.SYNCH
+                    };
+                    return MessageStatusReplyMessage;
+                }
+                else
+                {
+                    MessageStatusReplyMessage = new ReplyMessage()
+                    {
+                        MessageId = messageInfo.MessageId,
+                        ReplyMode = ReplyModeEnum.CAST
+                    };
+                }
             }
-            else
+            catch (Exception ex)
             {
                 MessageStatusReplyMessage = new ReplyMessage()
                 {
                     MessageId = messageInfo.MessageId,
-                    ReplyMode = ReplyModeEnum.CAST
+                    ReplyMode = ReplyModeEnum.CAST,
+                    ReplyAlertMessage=ex.Message
                 };
             }
             return MessageStatusReplyMessage;
-		}
+        }
 
-		/// <summary>
-		/// 应答消息数据部分保存到数据通道
-		/// </summary>
-        private bool ReplyToDataChannel(ReplyMessage replyMessageInfo,RequestMessage messageInfo,out string channelId)
+        /// <summary>
+        /// 应答消息数据部分保存到数据通道
+        /// </summary>
+        private bool ReplyToDataChannel(ReplyMessage replyMessageInfo, RequestMessage messageInfo, out string channelId)
         {
             channelId = string.Empty;
             //与数据通道连接，将MessageInfo中的数据部分存储到数据通道中，
@@ -82,32 +94,24 @@ namespace Victop.Frame.Connection
             ReplyMessage ReplyMessage = new ReplyMessage();
             DataCreateManager dataCreateManager = new DataCreateManager();
             bool result = false;
-            try
+            ReplyMessage = dataCreateManager.SendReplyMessage(replyMessageInfo, messageInfo);
+            channelId = ReplyMessage.DataChannelId;
+            if (string.IsNullOrEmpty(channelId))
             {
-                ReplyMessage = dataCreateManager.SendReplyMessage(replyMessageInfo, messageInfo);
-                channelId = ReplyMessage.DataChannelId;
-                if (string.IsNullOrEmpty(channelId)) 
-                {
-                    result = false;
-                }
-                else
-                {
-                    result = true;
-                }
+                result = false;
             }
-            catch (Exception)
+            else
             {
-
-                return false;
+                result = true;
             }
             return result;
         }
 
-		/// <summary>
-		/// 更新核心类库服务注册表
-		/// </summary>
-		private bool RefreshRegisterServerList(List<RegisterServerInfo> registerServerList)
-		{
+        /// <summary>
+        /// 更新核心类库服务注册表
+        /// </summary>
+        private bool RefreshRegisterServerList(List<RegisterServerInfo> registerServerList)
+        {
             RegisterServerManager registerServerManager = new RegisterServerManager();
             bool result = false;
             try
@@ -122,13 +126,13 @@ namespace Victop.Frame.Connection
                 return false;
             }
             return result;
-		}
+        }
 
-		/// <summary>
-		/// 更新菜单资源信息表
-		/// </summary>
-		public virtual void RefreshBaseResouceList(ReplyMessage messageInfo)
-		{
+        /// <summary>
+        /// 更新菜单资源信息表
+        /// </summary>
+        public virtual void RefreshBaseResouceList(ReplyMessage messageInfo)
+        {
             BaseResourceManager baseResourceManager = new BaseResourceManager();
             BaseResourceInfo baseResourceInfo = new BaseResourceInfo() { GalleryId = GalleryManager.GetCurrentGalleryId(), ResourceXml = messageInfo.ReplyContent };
             bool result = baseResourceManager.AddResouce(baseResourceInfo);
@@ -136,22 +140,22 @@ namespace Victop.Frame.Connection
             {
                 //TODO:提取基础资源信息xml中的服务部分，形成一个List集合
                 List<RegisterServerInfo> serverInfoList = new List<RegisterServerInfo>();
-                Dictionary<string,BaseResourceInfo> resourceInfoDic= baseResourceManager.GetAllBaseResource();
+                Dictionary<string, BaseResourceInfo> resourceInfoDic = baseResourceManager.GetAllBaseResource();
                 foreach (string key in resourceInfoDic.Keys)
                 {
                     RegisterServerInfo registerServerInfo = new RegisterServerInfo();
                     string messageType = JsonHelper.ReadJsonString(resourceInfoDic[key].ResourceXml, "messageType");
                     RegisterServerManager serverManager = new RegisterServerManager();
-                    registerServerInfo=serverManager.GetServer(GalleryManager.GetCurrentGalleryId().ToString(), messageType);
+                    registerServerInfo = serverManager.GetServer(GalleryManager.GetCurrentGalleryId().ToString(), messageType);
                     if (registerServerInfo != null)
                     {
                         serverInfoList.Add(registerServerInfo);
                     }
-                    
+
                 }
                 RefreshRegisterServerList(serverInfoList);
             }
-		}
+        }
         /// <summary>
         /// 根据数据通道id获取数据的xml
         /// </summary>
@@ -186,6 +190,6 @@ namespace Victop.Frame.Connection
             return createManger.CommitChannelData(channelId);
         }
 
-	}
+    }
 }
 
