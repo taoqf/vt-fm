eac90887baed9f4de6af328c4fe90d15919afe90
wangzhaoguang <wangzhaoguang@victop.com>
2014-08-19 17:31:32 +0800

    核心接口类增加数据类型枚举

---
+++ Victop.Frame.CoreLibrary/Enums/DataFormEnum.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Victop.Frame.CoreLibrary.Enums
+{
+    /// <summary>
+    /// 数据类型枚举
+    /// </summary>
+    public enum DataFormEnum
+    {
+        /// <summary>
+        /// 数据集合
+        /// </summary>
+        DATASET,
+        /// <summary>
+        /// JSON类型
+        /// </summary>
+        JSON
+    }
+}
