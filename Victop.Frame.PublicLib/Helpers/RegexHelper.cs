737999df2a3601d5f7192eea2bd81c00ee5116ff
wangzhaoguang <wangzhaoguang@victop.com>
2014-09-17 08:45:49 +0800

    增加正则表达式管理类

---
+++ Victop.Frame.PublicLib/Helpers/RegexHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Victop.Frame.PublicLib.Helpers
+{
+    public static class RegexHelper
+    {
+        /// <summary>
+        /// 以匹配字符串开始
+        /// </summary>
+        /// <param name="regexStr"></param>
+        /// <returns></returns>
+        public static string StartWith(string regexStr)
+        {
+            regexStr = string.Format("^{0}.*$", regexStr);
+            return regexStr;
+        }
+        /// <summary>
+        /// 以匹配字符串结尾
+        /// </summary>
+        /// <param name="regexStr"></param>
+        /// <returns></returns>
+        public static string EndWith(string regexStr)
+        {
+            regexStr = string.Format("^.*{0}$", regexStr);
+            return regexStr;
+        }
+        /// <summary>
+        /// 包含字符串
+        /// </summary>
+        /// <param name="regexStr"></param>
+        /// <returns></returns>
+        public static string Contains(string regexStr)
+        {
+            regexStr = string.Format("^.*{0}.*$", regexStr);
+            return regexStr;
+        }
+    }
+}
