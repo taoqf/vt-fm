e81bc50b750dda9e1088a632571e37b004c1e45e
zijiaxing <zijiaxing@victop.com>
2014-10-23 15:37:16 +0800

    增加两款皮肤项目

---
+++ Victop.Themes.WaterSkin/Skin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Victop.Themes.WaterSkin
+{
+    public class Skin
+    {
+        /// <summary>
+        /// 皮肤路径
+        /// </summary>
+        public int SkinOrder = 0;
+
+        /// <summary>
+        /// 皮肤名称
+        /// </summary>
+        public string SkinName = "默认皮肤";
+
+        /// <summary>
+        /// 皮肤xaml文件
+        /// </summary>
+        public string ThemeName = "/Victop.Themes.WaterSkin;component/Styles.xaml";
+    }
+}
