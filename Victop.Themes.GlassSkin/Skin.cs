fd849a1fd30d9a8050b8542746375868521c01ce
yuanxiaoping <yuanxiaoping@victop.com>
2014-10-27 12:25:17 +0800

    玻璃皮肤诞生……

---
+++ Victop.Themes.GlassSkin/Skin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Victop.Themes.GlassSkin
+{
+    public class Skin
+    {
+           /// <summary>
+            /// 皮肤路径
+            /// </summary>
+            public int SkinOrder =2;
+
+            /// <summary>
+            /// 皮肤名称
+            /// </summary>
+            public string SkinName = "玻璃";
+
+            /// <summary>
+            /// 皮肤xaml文件
+            /// </summary>
+            public string ThemeName = "/Victop.Themes.GlassicSkin;component/Styles.xaml";
+       
+    }
+}
