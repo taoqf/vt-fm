2d536c0c482407fda4fbab6e942972d3f09af9ab
zijiaxing <zijiaxing@victop.com>
2014-10-28 16:45:29 +0800

    修改弹出窗体按钮样式

--- AreaManagerPlugin/Views/UCAreaWindow.xaml.cs
+++ AreaManagerPlugin/Views/UCAreaWindow.xaml.cs
@@ -62,7 +62,6 @@ namespace AreaManagerPlugin.Views
             testGrid1.ItemsSource = dt.DefaultView;
             testGrid2.ItemsSource = dt.DefaultView;
             testGrid3.ItemsSource = dt.DefaultView;
-            testGrid4.ItemsSource = dt.DefaultView;
         }
     }
 }
