74372dfd6aa6670b63180af931077ab6db3a1500
zijiaxing <zijiaxing@victop.com>
2014-11-20 15:42:35 +0800

    修复窗体大小改变时左侧菜单栏显示位置BUG

---
+++ VictopPartner/HorizontalOffsetConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace VictopPartner
+{
+    public class HorizontalOffsetConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value != null && parameter != null)
+            {
+                return System.Convert.ToDouble(value) + System.Convert.ToDouble(parameter);
+            }
+            else
+            {
+                return value;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
