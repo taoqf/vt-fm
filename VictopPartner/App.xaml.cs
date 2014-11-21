a9928d434144ea5099644659a25fa00376ed629f
zijiaxing <zijiaxing@victop.com>
2014-11-21 15:07:37 +0800

    修改窗体图标

--- VictopPartner/App.xaml.cs
+++ VictopPartner/App.xaml.cs
@@ -5,7 +5,10 @@ using System.Data;
 using System.Linq;
 using System.Reflection;
 using System.Windows;
+using Victop.Frame.CoreLibrary;
 using Victop.Frame.PublicLib.Helpers;
+using Victop.Frame.SyncOperation;
+using Victop.Server.Controls;
 
 namespace VictopPartner
 {
@@ -14,5 +17,97 @@ namespace VictopPartner
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            if (FrameInit.GetInstance().FrameRun())
+            {
+                try
+                {
+                    this.GetCurrentSkin();
+                    string mainPlugin = ConfigurationManager.AppSettings["portalWindow"];
+                    Assembly pluginAssembly = ServerFactory.GetServerAssemblyByName(mainPlugin, "");
+                    Type[] types = pluginAssembly.GetTypes();
+                    foreach (Type t in types)
+                    {
+                        if (IsValidPlugin(t))
+                        {
+                            IPlugin plugin = (IPlugin)pluginAssembly.CreateInstance(t.FullName);
+                            plugin.StartWindow.WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
+                            plugin.StartWindow.ShowDialog();
+                            FrameInit.GetInstance().FrameUnload();
+                            Environment.Exit(0);
+                            break;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    FrameInit.GetInstance().FrameUnload();
+                    Environment.Exit(0);
+                }
+            }
+        }
+
+        private string AnonymousLogin()
+        {
+            Dictionary<string, string> messageDic = new Dictionary<string, string>();
+            messageDic.Add("MessageType", "LoginService.getCurrentLinker");
+            Dictionary<string, string> contentDic = new Dictionary<string, string>();
+            contentDic.Add("usercode", "test7");
+            messageDic.Add("MessageContent", JsonHelper.ToJson(contentDic));
+            return JsonHelper.ToJson(messageDic);
+        }
+
+        /// <summary>
+        /// 是否为有效的插件
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        private bool IsValidPlugin(Type type)
+        {
+            bool ret = false;
+            Type[] interfaces = type.GetInterfaces();
+            foreach (Type theInterface in interfaces)
+            {
+                if (theInterface.FullName == "Victop.Server.Controls.IPlugin")
+                {
+                    ret = true;
+                    break;
+                }
+            }
+            return ret;
+        }
+
+        #region 换肤
+
+        /// <summary>
+        /// 获取当前皮肤
+        /// </summary>
+        private void GetCurrentSkin()
+        {
+            if (ConfigurationManager.AppSettings.AllKeys.Contains("skinurl") && string.IsNullOrWhiteSpace(ConfigurationManager.AppSettings.Get("skinurl")) == false)
+            {
+                string skinNamespace = System.IO.Path.GetFileNameWithoutExtension(ConfigurationManager.AppSettings.Get("skinurl"));//得到皮肤命名空间
+                this.ChangeFrameWorkTheme("/" + skinNamespace + ";component/Styles.xaml", ConfigurationManager.AppSettings.Get("skinurl"));
+            }
+        }
+
+        /// <summary>
+        /// 主题皮肤改变发送消息
+        /// </summary>
+        private void ChangeFrameWorkTheme(string ThemeName, string SkinPath)
+        {
+            string messageType = "ServerCenterService.ChangeThemeByDll";
+            Dictionary<string, object> contentDic = new Dictionary<string, object>();
+            Dictionary<string, string> ServiceParams = new Dictionary<string, string>();
+            ServiceParams.Add("SourceName", ThemeName);
+            ServiceParams.Add("SkinPath", SkinPath);
+            contentDic.Add("ServiceParams", JsonHelper.ToJson(ServiceParams));
+            MessageOperation messageOp = new MessageOperation();
+            messageOp.SendMessage(messageType, contentDic);
+        }
+        #endregion
     }
 }
