1e68ae0ffc25af054e8c4d858d805e027f770105
wangzhaoguang <wangzhaoguang@victop.com>
2014-11-24 10:26:30 +0800

    封装范围查询、IN查询、不等查询

--- Victop.Frame.PublicLib/Helpers/RegexHelper.cs
+++ Victop.Frame.PublicLib/Helpers/RegexHelper.cs
@@ -40,5 +40,57 @@ namespace Victop.Frame.PublicLib.Helpers
             regexStr = string.Format("^.*{0}.*$", regexStr);
             return regexStr;
         }
+        /// <summary>
+        /// 整形范围(包含边界值)
+        /// </summary>
+        /// <param name="minValue"></param>
+        /// <param name="maxValue"></param>
+        /// <returns></returns>
+        public static Dictionary<string, object> longRange(long minValue, long maxValue)
+        {
+            Dictionary<string, object> resultDic = new Dictionary<string, object>();
+            resultDic.Add("$gte", minValue);
+            resultDic.Add("$lte", maxValue);
+            return resultDic;
+        }
+        /// <summary>
+        /// 时间范围(包含边界值)
+        /// </summary>
+        /// <param name="startTime"></param>
+        /// <param name="endTime"></param>
+        /// <returns></returns>
+        public static Dictionary<string, object> TimeRange(string startTimeStr, string endTimeStr)
+        {
+            Dictionary<string, object> resultDic = new Dictionary<string, object>();
+            DateTime zeroTime = new DateTime(1970, 1, 1);
+            DateTime startTime = Convert.ToDateTime(startTimeStr);
+            DateTime endTime = Convert.ToDateTime(endTimeStr);
+            resultDic.Add("$gte", (long)(startTime-zeroTime).TotalMilliseconds);
+            resultDic.Add("$lte", (long)(endTime - zeroTime).TotalMilliseconds);
+            return resultDic;
+        }
+        /// <summary>
+        /// 检索值In
+        /// </summary>
+        /// <param name="valueList"></param>
+        /// <returns></returns>
+        public static Dictionary<string, object> ValuesIn(List<object> valueList)
+        {
+            Dictionary<string, object> resultDic = new Dictionary<string, object>();
+            resultDic.Add("$in", valueList);
+            return resultDic;
+        }
+        /// <summary>
+        /// 不等于
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static Dictionary<string, object> ValueNoEqual(object value)
+        {
+            Dictionary<string, object> resultDic = new Dictionary<string, object>();
+            resultDic.Add("$ne", value);
+            return resultDic;
+        }
+
     }
 }
