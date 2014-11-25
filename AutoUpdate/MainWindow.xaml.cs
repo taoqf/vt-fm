b0c95ae957ed94e864297e3834694d8ad8d9d491
wangzhaoguang <wangzhaoguang@victop.com>
2014-11-25 13:35:36 +0800

    更新自动更新

--- AutoUpdate/MainWindow.xaml.cs
+++ AutoUpdate/MainWindow.xaml.cs
@@ -1,7 +1,13 @@
-﻿using System;
+﻿using AutoUpdate.Models;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
 using System.Diagnostics;
+using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +18,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml;
 
 namespace AutoUpdate
 {
@@ -20,13 +27,217 @@ namespace AutoUpdate
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region 字段
+        AutoUpdateModel updateModel = new AutoUpdateModel();
+        WebClient updateClient = new WebClient();
+        #endregion
         public MainWindow()
         {
             InitializeComponent();
+            this.Loaded += updateWindow_Loaded;
         }
 
         private void updateWindow_Loaded(object sender, RoutedEventArgs e)
         {
+            Process[] process = Process.GetProcessesByName("VictopPartner.exe");
+            foreach (Process item in process)
+            {
+                item.Kill();
+            }
+            InitUpdateInfo();
         }
+        /// <summary>
+        /// 初始化更新信息
+        /// </summary>
+        private void InitUpdateInfo()
+        {
+            updateModel.UpdateUrl = ConfigurationManager.AppSettings["UpdateUrl"];
+            updateModel.LocalUpdateTimestamp = Convert.ToInt64(ConfigurationManager.AppSettings["UpDate"]);
+            GetTheLastUpdateTime();
+            GetUpdateSize();
+            if (updateModel.LocalUpdateTimestamp != 0 && updateModel.ServerUpdateTimestamp != 0)
+            {
+                if (updateModel.ServerUpdateTimestamp > updateModel.LocalUpdateTimestamp)
+                {
+                    updateClient.DownloadProgressChanged += updateClient_DownloadProgressChanged;
+                    updateClient.DownloadFileCompleted += updateClient_DownloadFileCompleted;
+                }
+            }
+            if (updateModel.TotalSize == 0)
+            {
+                this.Close();
+            }
+            GetUpdateFileList();
+            if (updateModel.FileNames.Count > 0)
+            {
+                updateModel.UpdatedNum = 0;
+                DownLoadFile();
+            }
+        }
+
+        private void GetUpdateFileList()
+        {
+            try
+            {
+                updateModel.FileNames = new List<string>();
+                string AutoUpdaterFileName = updateModel.UpdateUrl + "AutoUpdater/AutoUpdater.xml";
+                WebClient wc = new WebClient();
+                Stream sm = wc.OpenRead(AutoUpdaterFileName);
+                XmlTextReader xr = new XmlTextReader(sm);
+                while (xr.Read())
+                {
+                    if (xr.Name == "FileInfo")
+                    {
+                        string fileInfo = xr.GetAttribute("Name");
+                        updateModel.FileNames.Add(fileInfo);
+                    }
+                }
+                xr.Close();
+                sm.Close();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        private void GetUpdateSize()
+        {
+            string AutoUpdaterFileName = updateModel.UpdateUrl + "AutoUpdater/AutoUpdater.xml";
+            try
+            {
+                WebClient wc = new WebClient();
+                Stream sm = wc.OpenRead(AutoUpdaterFileName);
+                XmlTextReader xr = new XmlTextReader(sm);
+                while (xr.Read())
+                {
+                    if (xr.Name == "UpdateFileList")
+                    {
+                        updateModel.TotalSize = Convert.ToInt64(xr.GetAttribute("Size"), CultureInfo.InvariantCulture);
+                        break;
+                    }
+                }
+                xr.Close();
+                sm.Close();
+            }
+            catch (WebException ex)
+            {   
+            }
+        }
+        /// <summary>
+        /// 文件下载完毕时
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void updateClient_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            if (File.Exists(AppDomain.CurrentDomain.BaseDirectory + "\\" + updateModel.LoadingFileName))
+            {
+                File.Delete(AppDomain.CurrentDomain.BaseDirectory + "\\" + updateModel.LoadingFileName);
+            }
+            File.Move(AppDomain.CurrentDomain.BaseDirectory + "\\AutoUpdater\\" + updateModel.LoadingFileName, AppDomain.CurrentDomain.BaseDirectory + "\\" + updateModel.LoadingFileName);
+            updateModel.UpdateSize += updateModel.LoadingFileSize;
+            if (updateModel.FileNames.Count > updateModel.UpdatedNum)
+            {
+                try
+                {
+                    updateModel.UpdatedNum++;
+                    DownLoadFile();
+                }
+                catch (Exception ex)
+                {
+                    
+                }
+            }
+            else
+            {
+                Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                config.AppSettings.Settings["UpDate"].Value = updateModel.ServerUpdateTimestamp.ToString();
+                config.Save(ConfigurationSaveMode.Modified);
+                ConfigurationManager.RefreshSection("appSettings");
+                this.Close();
+            }
+        }
+
+        private void DownLoadFile()
+        {
+            updateModel.LoadingFileName = updateModel.FileNames[(int)updateModel.UpdatedNum];
+            tBlockNow.Text = string.Format("更新进度 {0}/{1}  [ {2} ]", updateModel.UpdatedNum, updateModel.TotalCount, ConvertSize(updateModel.TotalSize));
+            proBarTotal.Value = 0;
+            string directoryPath = AppDomain.CurrentDomain.BaseDirectory + "\\AutoUpdater\\";
+            if (!Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+            updateClient.DownloadFileAsync(new Uri(updateModel.UpdateUrl + "AutoUpdater/" + updateModel.LoadingFileName), directoryPath + updateModel.LoadingFileName);
+        }
+        /// <summary>
+        /// 下载进行时
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void updateClient_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            tBlockProcess.Text = string.Format("正在下载:{0} [{1}/{2}]", updateModel.LoadingFileName, ConvertSize(e.BytesReceived), ConvertSize(e.TotalBytesToReceive));
+            updateModel.LoadingFileSize = e.TotalBytesToReceive;
+            float tempF = ((float)(updateModel.UpdateSize + e.BytesReceived) / updateModel.TotalSize);
+            proBarNow.Value = Convert.ToInt32(tempF * 100);
+            proBarTotal.Value = e.ProgressPercentage;
+        }
+
+        /// <summary> 
+        /// 判断软件的更新日期 
+        /// </summary> 
+        /// <param name="Dir">服务器地址</param> 
+        /// <returns>返回日期</returns> 
+        private void GetTheLastUpdateTime()
+        {
+            string AutoUpdaterFileName = updateModel.UpdateUrl + "AutoUpdater/AutoUpdater.xml";
+            try
+            {
+                WebClient wc = new WebClient();
+                Stream sm = wc.OpenRead(AutoUpdaterFileName);
+                XmlTextReader xml = new XmlTextReader(sm);
+                while (xml.Read())
+                {
+                    if (xml.Name == "UpdateTime")
+                    {
+                        updateModel.ServerUpdateTimestamp = Convert.ToInt64(xml.GetAttribute("Date"));
+                        break;
+                    }
+                }
+                xml.Close();
+                sm.Close();
+            }
+            catch (WebException ex)
+            {
+            }
+        }
+        /// <summary> 
+        /// 转换字节大小 
+        /// </summary> 
+        /// <param name="byteSize">输入字节数</param> 
+        /// <returns>返回值</returns> 
+        private static string ConvertSize(long byteSize)
+        {
+            string str = "";
+            float tempf = (float)byteSize;
+            if (tempf / 1024 > 1)
+            {
+                if ((tempf / 1024) / 1024 > 1)
+                {
+                    str = ((tempf / 1024) / 1024).ToString("##0.00", CultureInfo.InvariantCulture) + "MB";
+                }
+                else
+                {
+                    str = (tempf / 1024).ToString("##0.00", CultureInfo.InvariantCulture) + "KB";
+                }
+            }
+            else
+            {
+                str = tempf.ToString(CultureInfo.InvariantCulture) + "B";
+            }
+            return str;
+        } 
     }
 }
---
+++ AutoUpdate/Models/AutoUpdateModel.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AutoUpdate.Models
+{
+    public class AutoUpdateModel
+    {
+        /// <summary>
+        /// 更新地址
+        /// </summary>
+        private string updateUrl;
+        /// <summary>
+        /// 更新地址
+        /// </summary>
+        public string UpdateUrl
+        {
+            get { return updateUrl; }
+            set { updateUrl = value; }
+        }
+        /// <summary>
+        /// 本地更新时间
+        /// </summary>
+        private long localUpdateTimestamp;
+        /// <summary>
+        /// 本地更新时间
+        /// </summary>
+        public long LocalUpdateTimestamp
+        {
+            get { return localUpdateTimestamp; }
+            set { localUpdateTimestamp = value; }
+        }
+        /// <summary>
+        /// 服务端更新时间
+        /// </summary>
+        private long serverUpdateTimestamp;
+        /// <summary>
+        /// 服务端更新时间
+        /// </summary>
+        public long ServerUpdateTimestamp
+        {
+            get { return serverUpdateTimestamp; }
+            set { serverUpdateTimestamp = value; }
+        }
+        /// <summary>
+        /// 下载文件名称
+        /// </summary>
+        private string loadingFileName;
+        /// <summary>
+        /// 下载文件名称
+        /// </summary>
+        public string LoadingFileName
+        {
+            get { return loadingFileName; }
+            set { loadingFileName = value; }
+        }
+        /// <summary>
+        /// 当前文件大小
+        /// </summary>
+        private long loadingFileSize;
+        /// <summary>
+        /// 当前文件大小
+        /// </summary>
+        public long LoadingFileSize
+        {
+            get { return loadingFileSize; }
+            set { loadingFileSize = value; }
+        }
+        /// <summary>
+        /// 所有文件大小
+        /// </summary>
+        private long totalSize;
+        /// <summary>
+        /// 所有文件大小
+        /// </summary>
+        public long TotalSize
+        {
+            get { return totalSize; }
+            set { totalSize = value; }
+        }
+        /// <summary>
+        /// 文件总数
+        /// </summary>
+        private long totalCount;
+        /// <summary>
+        /// 文件总数
+        /// </summary>
+        public long TotalCount
+        {
+            get { return totalCount; }
+            set { totalCount = value; }
+        }
+        /// <summary>
+        /// 文件集合
+        /// </summary>
+        private List<string> fileNames;
+        /// <summary>
+        /// 文件集合
+        /// </summary>
+        public List<string> FileNames
+        {
+            get { return fileNames; }
+            set { fileNames = value; }
+        }
+        /// <summary>
+        /// 已更新文件数
+        /// </summary>
+        private long updatedNum;
+        /// <summary>
+        /// 已更新文件数
+        /// </summary>
+        public long UpdatedNum
+        {
+            get { return updatedNum; }
+            set { updatedNum = value; }
+        }
+        /// <summary>
+        /// 已更新文件大小
+        /// </summary>
+        private long updateSize;
+        /// <summary>
+        /// 已更新文件大小
+        /// </summary>
+        public long UpdateSize
+        {
+            get { return updateSize; }
+            set { updateSize = value; }
+        }
+    }
+}
