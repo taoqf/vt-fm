d1a3492e9d26ceba4c1174318059a53f046d8fef
wangzhaoguang <wangzhaoguang@victop.com>
2014-12-12 14:14:08 +0800

    调整组件运行时项目

--- Victop.Component.Runtime/DefinViewsModel.cs
+++ Victop.Component.Runtime/DefinViewsModel.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Newtonsoft.Json;
+using Victop.Frame.DataChannel;
+using Victop.Frame.PublicLib.Helpers;
+using Victop.Frame.SyncOperation;
 
 namespace Victop.Component.Runtime
 {
@@ -56,12 +59,59 @@ namespace Victop.Component.Runtime
             get;
             set;
         }
+
         /// <summary>
-        /// 重构View下所有Block
+        /// 获取Block数据
         /// </summary>
-        public void RebuildAllDataPath()
+        public void GetBlockData(string BlockName)
         {
- 
+            if (!string.IsNullOrEmpty(BlockName))
+            {
+                ViewsBlockModel blockModel = Blocks.FirstOrDefault(it => it.BlockName == BlockName);
+                if (blockModel != null && blockModel.BlockDataPath != null && blockModel.BlockDataPath.Count > 0)
+                {
+                    if (!string.IsNullOrEmpty(ViewId))
+                    {
+                        DataOperation dataOp = new DataOperation();
+                        blockModel.BlockDt = dataOp.GetData(ViewId, JsonHelper.ToJson(blockModel.BlockDataPath));
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// 保存数据
+        /// </summary>
+        public void SaveData()
+        {
+            if (!string.IsNullOrEmpty(ViewId))
+            {
+                ViewsBlockModel blockmodel = Blocks.Find(it => it.Superiors.Equals("root"));
+                if (blockmodel.BlockDataPath != null && blockmodel.BlockDataPath.Count > 0)
+                {
+                    DataOperation dataOp = new DataOperation();
+                    dataOp.SaveData(ViewId, JsonHelper.ToJson(blockmodel.BlockDataPath));
+                    SaveBlockData(ViewId, blockmodel);
+                }
+            }
+        }
+        private void SaveBlockData(string viewId, ViewsBlockModel blockModel)
+        {
+            List<ViewsBlockModel> BlockList = Blocks.Where(it => it.Superiors.Equals(blockModel.BlockName)).ToList();
+            if (BlockList != null && BlockList.Count > 0)
+            {
+                foreach (ViewsBlockModel item in BlockList)
+                {
+                    if (item.BlockDataPath != null && item.BlockDataPath.Count > 0)
+                    {
+                        DataOperation dataOp = new DataOperation();
+                        dataOp.SaveData(viewId, JsonHelper.ToJson(item.BlockDataPath));
+                    }
+                }
+                foreach (ViewsBlockModel item in BlockList)
+                {
+                    SaveBlockData(viewId, item);
+                }
+            }
         }
     }
 }
