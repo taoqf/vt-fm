0a1dc1649c33d62baa181a3adc775bd54ebd0282
wangzhaoguang <wangzhaoguang@victop.com>
2014-12-12 13:34:24 +0800

    添加组件运行时项目

---
+++ Victop.Component.Runtime/ComponentModel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Victop.Component.Runtime
+{
+    /// <summary>
+    /// 组件定义
+    /// </summary>
+    public class ComponentModel
+    {
+        /// <summary>
+        /// 组件名称
+        /// </summary>
+        [JsonProperty(PropertyName = "componentName")]
+        public string ComponentName
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// 组件类型
+        /// </summary>
+        [JsonProperty(PropertyName = "compoType")]
+        public string CompoType
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// 组件编号
+        /// </summary>
+        [JsonProperty(PropertyName = "component_No")]
+        public string ComponentNo
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// 组件定义
+        /// </summary>
+        [JsonProperty(PropertyName = "compntDefin")]
+        public CompntDefinModel CompntDefin
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// 组件设置
+        /// </summary>
+        [JsonIgnore]
+        public CompntSettingModel CompntSettings
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// 组件总参表
+        /// </summary>
+        [JsonIgnore]
+        public Dictionary<string, Dictionary<string, object>> RuntimeParams
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ Victop.Component.Runtime/DefinParamMapModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Victop.Component.Runtime
+{
+    /// <summary>
+    /// 组件定义-参数映射实体
+    /// </summary>
+    public class DefinParamMapModel
+    {
+        /// <summary>
+        /// 插件名称
+        /// </summary>
+        [JsonProperty(PropertyName = "pluginName")]
+        public string PluginName
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// 插件参数名称
+        /// </summary>
+        [JsonProperty(PropertyName = "paramName")]
+        public string ParamName
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// 组件参数名称
+        /// </summary>
+        [JsonProperty(PropertyName = "compoParam")]
+        public string CompoParam
+        {
+            get;
+            set;
+        }
+    }
+}
