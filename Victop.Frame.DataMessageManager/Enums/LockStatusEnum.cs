0a5047fa07fcdee5e6d0786efc1bff7819667c1a
wangzhaoguang <wangzhaoguang@victop.com>
2015-03-26 18:53:50 +0800

    增加数据锁请求及解锁方法，框架关闭时清空数据锁

---
+++ Victop.Frame.DataMessageManager/Enums/LockStatusEnum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Victop.Frame.DataMessageManager.Enums
+{
+    public enum LockStatusEnum
+    {
+        锁定 = 1,
+        解锁 = 2
+    }
+}
---
+++ Victop.Frame.DataMessageManager/StaticClass/DataLockInfoClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Victop.Frame.DataMessageManager.Models;
+
+namespace Victop.Frame.DataMessageManager.StaticClass
+{
+    internal static class DataLockInfoClass
+    {
+        private static List<LockInfoModel> lockInfoList;
+
+        internal static List<LockInfoModel> LockInfoList
+        {
+            get
+            {
+                if (DataLockInfoClass.lockInfoList == null)
+                    DataLockInfoClass.lockInfoList = new List<LockInfoModel>();
+                return DataLockInfoClass.lockInfoList;
+            }
+            set
+            {
+                DataLockInfoClass.lockInfoList = value;
+            }
+        }
+    }
+}
