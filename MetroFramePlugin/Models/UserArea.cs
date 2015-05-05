463452f93e9a466ae0cf64e03c464cf16632d777
yuanxiaoping <yuanxiaoping@ifeidao.com>
2015-05-05 14:19:29 +0800

    MetroFramePlugin中添加新UserArea类。

---
+++ MetroFramePlugin/Models/UserArea.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using Victop.Server.Controls.Models;
+
+namespace MetroFramePlugin.Models
+{
+    public class UserArea : PropertyModelBase
+    {
+        /// <summary>
+        ///用户Coder
+        /// </summary>
+        private string userCode;
+        public string UserCode
+        {
+            get { return userCode; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value) == false)
+                {
+                    userCode = value;
+                    RaisePropertyChanged("UserCode");
+                }
+            }
+        }
+        /// <summary>
+        /// 区域菜单
+        /// </summary>
+        private ObservableCollection<AreaMenu> userAreaMenu;
+        public ObservableCollection<AreaMenu> UserAreaMenu
+        {
+            get
+            {
+                if (userAreaMenu == null)
+                    userAreaMenu = new ObservableCollection<AreaMenu>();
+                return userAreaMenu;
+            }
+            set
+            {
+                if (userAreaMenu != value)
+                {
+                    userAreaMenu = value;
+                    RaisePropertyChanged("UserAreaMenu");
+                }
+            }
+        }
+    }
+}
