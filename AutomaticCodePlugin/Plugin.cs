74495b0e060ea97bba3b4beee8439d80633a8b89
wangzhaoguang <wangzhaoguang@ifeidao.com>
2015-10-23 09:55:50 +0800

    添加自动代码插件，dll文件中增加状态机与规则引擎库

---
+++ AutomaticCodePlugin/Plugin.cs
@@ -0,0 +1,77 @@
+﻿using AutomaticCodePlugin.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using Victop.Server.Controls;
+
+namespace AutomaticCodePlugin
+{
+    public class Plugin:IPlugin
+    {
+        private string pluginTitle = "自动代码插件";
+        public string PluginTitle
+        {
+            get { return pluginTitle; }
+            set { pluginTitle = value; }
+        }
+
+        public string PluginName
+        {
+            get { return "AutomaticCodePlugin"; }
+        }
+
+        public List<string> ServiceReceiptMessageType
+        {
+            get { return new List<string>(); }
+        }
+
+        public int ShowType
+        {
+            get { return 1; }
+        }
+
+        public int SystemPlugin
+        {
+            get { return 1; }
+        }
+
+        public int AutoInit
+        {
+            get { return 1; }
+        }
+
+        public Window StartWindow
+        {
+            get { return new MainViewWindow(); }
+        }
+
+        public UserControl StartControl
+        {
+            get
+            {
+                return new UCMainView();
+            }
+        }
+
+        public void Init()
+        {
+        }
+
+        public Dictionary<string, object> ParamDict
+        {
+            get
+            {
+                return UCMainView.ParamDict;
+
+            }
+            set
+            {
+                UCMainView.ParamDict = value;
+            }
+        }
+    }
+}
---
+++ AutomaticCodePlugin/Views/MainViewWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AutomaticCodePlugin.Views
+{
+    /// <summary>
+    /// MainViewWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainViewWindow : Window
+    {
+        public MainViewWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
