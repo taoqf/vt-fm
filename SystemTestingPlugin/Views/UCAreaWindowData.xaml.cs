8ea4842a7a172195a448a9c06f50584ba31edb91
wangzhaoguang <wangzhaoguang@ifeidao.com>
2015-12-11 17:09:10 +0800

    更改测试插件的界面基类为TemplateControl

--- SystemTestingPlugin/Views/UCAreaWindowData.xaml.cs
+++ SystemTestingPlugin/Views/UCAreaWindowData.xaml.cs
@@ -11,24 +11,18 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Victop.Frame.CmptRuntime;
 
 namespace SystemTestingPlugin.Views
 {
     /// <summary>
     /// UCAreaWindowData.xaml 的交互逻辑
     /// </summary>
-    public partial class UCAreaWindowData : UserControl
+    public partial class UCAreaWindowData : TemplateControl
     {
         public UCAreaWindowData()
         {
             InitializeComponent();
-        }
-        private static Dictionary<string, object> paramDict;
-
-        public static Dictionary<string, object> ParamDict
-        {
-            get { return paramDict; }
-            set { paramDict = value; }
         }
     }
 }
