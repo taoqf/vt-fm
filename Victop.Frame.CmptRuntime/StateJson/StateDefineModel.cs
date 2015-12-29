4c74392231b5d7294283769a69fd897d3976326a
wangzhaoguang <wangzhaoguang@ifeidao.com>
2015-12-29 10:14:01 +0800

    调整PVD实体结构

---
+++ Victop.Frame.CmptRuntime/StateJson/StateDefineModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Victop.Frame.CmptRuntime
+{
+    /// <summary>
+    /// 状态定义实体
+    /// </summary>
+    public class StateDefineModel
+    {
+        private object defEvents;
+        /// <summary>
+        /// 事件
+        /// </summary>
+        [JsonProperty(PropertyName = "events")]
+        public object DefEvents
+        {
+            get
+            {
+                return defEvents;
+            }
+            set
+            {
+                defEvents = value;
+            }
+        }
+
+        private object defStates;
+        /// <summary>
+        /// 状态
+        /// </summary>
+        [JsonProperty(PropertyName = "states")]
+        public object DefStates
+        {
+            get
+            {
+                return defStates;
+            }
+            set
+            {
+                defStates = value;
+            }
+        }
+        private object defTransitions;
+        /// <summary>
+        /// 转移
+        /// </summary>
+        [JsonProperty(PropertyName = "transitions")]
+        public object DefTransitions
+        {
+            get
+            {
+                return defTransitions;
+            }
+            set
+            {
+                defTransitions = value;
+            }
+        }
+    }
+}
