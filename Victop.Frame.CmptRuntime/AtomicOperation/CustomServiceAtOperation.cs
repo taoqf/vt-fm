e3f48b732ec15145de98abe2e77a9aa4dd59bb63
chenshengqiang <chenshengqiang@victop.com>
2016-01-20 11:30:21 +0800

    修改调用服务原子操作，增加oav存在先删除的逻辑

--- Victop.Frame.CmptRuntime/AtomicOperation/CustomServiceAtOperation.cs
+++ Victop.Frame.CmptRuntime/AtomicOperation/CustomServiceAtOperation.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using Victop.Frame.DataMessageManager;
 using Victop.Server.Controls.Models;
+using Victop.Frame.PublicLib.Helpers;
 
 namespace Victop.Frame.CmptRuntime.AtomicOperation
 {
@@ -16,6 +17,10 @@ namespace Victop.Frame.CmptRuntime.AtomicOperation
         /// 服务参数集合
         /// </summary>
         Dictionary<string, Dictionary<string, object>> serviceParamList = new Dictionary<string, Dictionary<string, object>>();
+        /// <summary>
+        /// 服务OAV集合
+        /// </summary>
+        Dictionary<string, List<OAVModel>> serviceOAVList = new Dictionary<string, List<OAVModel>>();
         /// <summary>
         /// 数据消息管理
         /// </summary>
@@ -71,12 +76,38 @@ namespace Victop.Frame.CmptRuntime.AtomicOperation
                 Dictionary<string, object> resultDic = messageOp.SendMessage(serviceName, serviceParamList[serviceName]);
                 if (resultDic != null)
                 {
+                    //已调用过先清除OAV,防止重复添加
+                    if (serviceOAVList.ContainsKey(serviceName))
+                    {
+                        try
+                        {
+                            foreach (OAVModel oav in serviceOAVList[serviceName])
+                            {
+                                MainView.FeiDaoFSM.RemoveFact(oav);
+                            }
+                            serviceOAVList[serviceName].Clear();
+                        }
+                        catch (Exception ex)
+                        {
+                            serviceOAVList[serviceName].Clear();
+                            LoggerHelper.Error("删除服务OAV失败!服务为：" + serviceName +"。错误原因"+ ex.ToString());
+                        }
+                    }
+                    else
+                    {
+                        serviceOAVList.Add(serviceName, new List<OAVModel>());
+                    }
                     OAVModel oavCode = new OAVModel(serviceName, "code", resultDic["ReplyMode"]);
                     OAVModel oavMessage = new OAVModel(serviceName, "message", resultDic["ReplyAlertMessage"]);
                     OAVModel oavContent = new OAVModel(serviceName, "content", resultDic["ReplyContent"]);
                     MainView.FeiDaoFSM.InsertFact(oavCode);
                     MainView.FeiDaoFSM.InsertFact(oavMessage);
                     MainView.FeiDaoFSM.InsertFact(oavContent);
+                    //oav存入集合
+                    serviceOAVList[serviceName].Add(oavCode);
+                    serviceOAVList[serviceName].Add(oavMessage);
+                    serviceOAVList[serviceName].Add(oavContent);
+                    
                 }
                 serviceParamList.Remove(serviceName);
             }
