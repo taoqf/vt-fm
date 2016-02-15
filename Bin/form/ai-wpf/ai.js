892111e365276c722ac6939362abb3c3ef126fe2
wangzhaoguang <wangzhaoguang@ifeidao.com>
2016-02-15 13:55:13 +0800

    新规则相关文件

---
+++ Bin/form/ai-wpf/ai.js
@@ -0,0 +1,8 @@
+(function(ai){(function(m){for(var j in m){ai[j] = m[j];}})((function(){return {
+	"js_method" : "wpf_test_method",
+	"SysConsole": "SysConsole",
+	"SetButtonText":"SetButtonText",
+	"ExcutePageTrigger":"ExcutePageTrigger",
+	"ExcuteComponentTrigger":"ExcuteComponentTrigger",
+	"GetPBlockData":"GetPBlockData"
+};})());})((function(){return window.ai_map || (window.ai_map = {})})());
